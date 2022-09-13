68d6ee12b17aa7107d51b9ec106b3e0bbfca998d
aplqo <aplqo@outlook.com>
2022-09-14 00:24:51 +0800

    [people] add pin, collection, favlists

--- src/ZhArchiver/People/API.hs
+++ src/ZhArchiver/People/API.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module ZhArchiver.People.API where
@@ -6,6 +7,7 @@ import qualified Data.Aeson as JSON
 import Data.List.NonEmpty
 import Data.Text (Text)
 import Text.URI
+import Text.URI.QQ
 import ZhArchiver.Request.Paging
 import ZhArchiver.Request.Uri
 
@@ -22,3 +24,30 @@ getColumnRaw uid =
     sp <- $(apiPath "members" "column-contributions") uid
     reqPaging
       (httpsURI sp [])
+
+getPinsRaw :: Text -> IO [JSON.Value]
+getPinsRaw uid =
+  do
+    sp <- $(apiPath "pins" "moments") uid
+    reqPaging
+      (httpsURI sp [])
+
+getCollectionsRaw :: Text -> IO [JSON.Value]
+getCollectionsRaw uid =
+  do
+    sp <- $(apiPath "people" "collections") uid
+    reqPaging
+      ( httpsURI
+          sp
+          [QueryParam [queryKey|include|] [queryValue|data[*].updated_time,answer_count,follower_count,creator,description,is_following,comment_count,created_time;data[*].creator.vip_info|]]
+      )
+
+getFollowingFavlistsRaw :: Text -> IO [JSON.Value]
+getFollowingFavlistsRaw uid =
+  do
+    sp <- $(apiPath "members" "following-favlists") uid
+    reqPaging
+      ( httpsURI
+          sp
+          [QueryParam [queryKey|include|] [queryValue|data[*].updated_time,answer_count,follower_count,creator,description,is_following,comment_count,created_time|]]
+      )
