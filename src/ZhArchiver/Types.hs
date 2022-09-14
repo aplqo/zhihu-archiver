12805402048755acec1c5bbfa91dba5d26482647
aplqo <aplqo@outlook.com>
2022-09-14 17:04:49 +0800

    [types] fix id json

--- src/ZhArchiver/Types.hs
+++ src/ZhArchiver/Types.hs
@@ -1,13 +1,3 @@
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+module ZhArchiver.Types (Id) where
 
-module ZhArchiver.Types (Id, mkId) where
-
-import Data.Aeson
-
-newtype Id = Id Int
-  deriving (Eq, Ord, Show)
-  deriving newtype (FromJSON, ToJSON)
-
-mkId :: Int -> Id
-mkId = Id
+type Id = Int
