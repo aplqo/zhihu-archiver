7113818aece294f93c2170b1765df452f89ee607
aplqo <aplqo@outlook.com>
2022-09-16 13:23:38 +0800

    [question] impl answer parser

--- src/ZhArchiver/Question.hs
+++ src/ZhArchiver/Question.hs
@@ -10,11 +10,11 @@ import Control.Monad.Catch (MonadThrow)
 import Data.Aeson hiding (Value)
 import qualified Data.Aeson as JSON
 import Data.Aeson.Types
+import Data.Int
 import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.Maybe (fromJust)
 import Data.Text (Text)
 import qualified Data.Text as T
-import Data.Time.LocalTime
 import GHC.Generics (Generic)
 import Network.HTTP.Req
 import qualified Network.HTTP.Req as R
@@ -32,7 +32,7 @@ import ZhArchiver.Types
 data Question = Question
   { qId :: Id,
     qAuthor :: Maybe Author,
-    qCreated, qUpdated :: ZonedTime,
+    qCreated, qUpdated :: Time,
     qContent :: Maybe Content,
     qComments :: [Comment],
     qRawData :: JSON.Value
@@ -105,6 +105,23 @@ fetchAnswersRaw qid =
           [QueryParam [queryKey|include|] [queryValue|data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,is_sticky,collapsed_by,suggest_edit,comment_count,can_comment,content,editable_content,attachment,voteup_count,reshipment_settings,comment_permission,created_time,updated_time,review_info,relevant_info,question,excerpt,is_labeled,paid_info,paid_info_content,reaction_instruction,relationship.is_authorized,is_author,voting,is_thanked,is_nothelp,is_recognized;data[*].mark_infos[*].url;data[*].author.follower_count,vip_info,badge[*].topics;data[*].settings.table_of_content.enabled|]]
       )
 
+data Answer = Answer
+  { aId :: Id,
+    aAuthor :: Maybe Author,
+    aCreated, aUpdated :: Time,
+    aVoteUp :: Int64,
+    aContent :: Content,
+    aComment :: [Comment],
+    aRawData :: JSON.Value
+  }
+  deriving (Show, Generic)
+
+instance FromJSON Answer where
+  parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = tail}
+
+instance ToJSON Answer where
+  toJSON = genericToJSON defaultOptions {fieldLabelModifier = tail}
+
 fetchAnswerRaw :: MonadHttp m => ZseState -> Id -> m JSON.Value
 fetchAnswerRaw zs aid =
   responseBody
@@ -113,5 +130,41 @@ fetchAnswerRaw zs aid =
       (R.https "www.zhihu.com" /: "api" /: "v4" /: "answers" /: T.pack (show aid))
       NoReqBody
       (jsonResponse @JSON.Value)
-      ("include" =: ("content;comment_count" :: T.Text))
+      ("include" =: ("content;comment_count;voteup_count" :: T.Text))
       (zse96 zs)
+
+parseRawAnswer :: JSON.Value -> (Answer, Int, Int)
+parseRawAnswer =
+  fromJust
+    . parseMaybe
+      ( \v ->
+          withObject
+            "answer"
+            ( \o -> do
+                aid <- o .: "id"
+                author <- o .: "author" >>= parseAuthor
+                created <- o .: "created_time" >>= parseTime
+                updated <- o .: "updated_time" >>= parseTime
+                vote <- o .: "voteup_count"
+                content <- o .: "content"
+
+                qid <- o .: "question" >>= withObject "question" (.: "id")
+                ccnt <- o .: "comment_count"
+
+                return
+                  ( Answer
+                      { aId = aid,
+                        aAuthor = author,
+                        aCreated = created,
+                        aUpdated = updated,
+                        aVoteUp = vote,
+                        aContent = Content {contHtml = content, contImages = emptyImgMap},
+                        aComment = [],
+                        aRawData = v
+                      },
+                    qid,
+                    ccnt
+                  )
+            )
+            v
+      )
