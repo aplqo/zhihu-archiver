b6a179862bce2efc62201dd159488e7bdefdd4bb
aplqo <aplqo@outlook.com>
2022-09-17 12:08:39 +0800

    [people] impl ItemContainer Answer, Article

--- src/ZhArchiver/Item/People.hs
+++ src/ZhArchiver/Item/People.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -9,12 +11,19 @@ import Data.Aeson.TH
 import Data.Text (Text)
 import qualified Data.Text as T
 import Network.HTTP.Req
+import Text.URI
+import Text.URI.QQ
 import ZhArchiver.Content
 import ZhArchiver.Image
-import ZhArchiver.Image.TH
+import ZhArchiver.Image.TH (deriveHasImage)
 import ZhArchiver.Item
+import ZhArchiver.Item.Answer (Answer)
+import ZhArchiver.Item.Article (Article)
 import ZhArchiver.RawParser.TH
 import ZhArchiver.RawParser.Util
+import ZhArchiver.Request.Paging
+import ZhArchiver.Request.Uri hiding (https)
+import ZhArchiver.Request.Zse96V3
 
 data People = People
   { pId, pUrlToken :: Text,
@@ -59,3 +68,30 @@ instance ZhData People where
          ]
      )
       v
+
+instance ItemContainer People Answer where
+  type ICSigner People Answer = ZseState
+  fetchItemsRaw zs (People {pId = uid}) = do
+    sp <- $(apiPath "members" "answers") uid
+    fmap Raw
+      <$> reqPagingSign
+        ( httpsURI
+            sp
+            [ QueryParam [queryKey|include|] [queryValue|data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,collapsed_by,suggest_edit,comment_count,can_comment,content,editable_content,attachment,voteup_count,reshipment_settings,comment_permission,mark_infos,created_time,updated_time,review_info,excerpt,is_labeled,label_info,relationship.is_authorized,voting,is_author,is_thanked,is_nothelp,is_recognized;data[*].vessay_info;data[*].author.badge[?(type=best_answerer)].topics;data[*].author.vip_info;data[*].question.has_publishing_draft,relationship|],
+              QueryParam [queryKey|limit|] [queryValue|20|],
+              QueryParam [queryKey|offset|] [queryValue|0|]
+            ]
+        )
+        (zse96 zs)
+
+instance ItemContainer People Article where
+  type ICSigner People Article = ZseState
+  fetchItemsRaw zs (People {pId = uid}) = do
+    sp <- $(apiPath "members" "articles") uid
+    fmap Raw
+      <$> reqPagingSign
+        ( httpsURI
+            sp
+            [QueryParam [queryKey|include|] [queryValue|data[*].comment_count,suggest_edit,is_normal,thumbnail_extra_info,thumbnail,can_comment,comment_permission,admin_closed_comment,content,voteup_count,created,updated,upvoted_followees,voting,review_info,is_labeled,label_info;data[*].vessay_info;data[*].author.badge[?(type=best_answerer)].topics;data[*].author.vip_info;|]]
+        )
+        (zse96 zs)
--- src/ZhArchiver/People/API.hs
+++ src/ZhArchiver/People/API.hs
@@ -21,31 +21,6 @@ getActivityRaw uid =
     reqPaging
       (httpsURI sp [])
 
-getAnswersRaw :: (MonadHttp m, MonadThrow m) => ZseState -> Text -> m [JSON.Value]
-getAnswersRaw zs uid =
-  do
-    sp <- $(apiPath "members" "answers") uid
-    reqPagingSign
-      ( httpsURI
-          sp
-          [ QueryParam [queryKey|include|] [queryValue|data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,collapsed_by,suggest_edit,comment_count,can_comment,content,editable_content,attachment,voteup_count,reshipment_settings,comment_permission,mark_infos,created_time,updated_time,review_info,excerpt,is_labeled,label_info,relationship.is_authorized,voting,is_author,is_thanked,is_nothelp,is_recognized;data[*].vessay_info;data[*].author.badge[?(type=best_answerer)].topics;data[*].author.vip_info;data[*].question.has_publishing_draft,relationship|],
-            QueryParam [queryKey|limit|] [queryValue|20|],
-            QueryParam [queryKey|offset|] [queryValue|0|]
-          ]
-      )
-      (zse96 zs)
-
-getArticlesRaw :: (MonadHttp m, MonadThrow m) => ZseState -> Text -> m [JSON.Value]
-getArticlesRaw zs uid =
-  do
-    sp <- $(apiPath "members" "articles") uid
-    reqPagingSign
-      ( httpsURI
-          sp
-          [QueryParam [queryKey|include|] [queryValue|data[*].comment_count,suggest_edit,is_normal,thumbnail_extra_info,thumbnail,can_comment,comment_permission,admin_closed_comment,content,voteup_count,created,updated,upvoted_followees,voting,review_info,is_labeled,label_info;data[*].vessay_info;data[*].author.badge[?(type=best_answerer)].topics;data[*].author.vip_info;|]]
-      )
-      (zse96 zs)
-
 getColumnRaw :: (MonadHttp m, MonadThrow m) => Text -> m [JSON.Value]
 getColumnRaw uid =
   do
