ddd53bac4d4b616805733b5e6302486aec648051
aplqo <aplqo@outlook.com>
2022-09-17 19:04:44 +0800

    [item] add util func

--- src/ZhArchiver/Item.hs
+++ src/ZhArchiver/Item.hs
@@ -4,22 +4,30 @@
 module ZhArchiver.Item
   ( RawData (..),
     ZhData (..),
+    runParser,
     Item (..),
     ItemContainer (..),
+    fetchItems,
   )
 where
 
 import Control.Monad.Catch
 import qualified Data.Aeson as JSON
-import Data.Aeson.Types (Parser)
+import Data.Aeson.Types
 import Network.HTTP.Req
-import ZhArchiver.Progress (Cli)
+import ZhArchiver.Progress
 
 newtype RawData a = Raw {unRaw :: JSON.Value}
   deriving (Show)
 
 class ZhData d where
   parseRaw :: RawData d -> Parser d
+
+runParser :: (ZhData a) => RawData a -> a
+runParser v =
+  case parse parseRaw v of
+    Success r -> r
+    Error e -> error ("Parse error:" ++ e)
 
 class (ZhData a) => Item a where
   type IId a
@@ -30,3 +38,14 @@ class (Item a, ZhData i) => ItemContainer a i where
   type ICOpt a i
   type ICSigner a i
   fetchItemsRaw :: (MonadHttp m, MonadThrow m) => Cli -> ICOpt a i -> ICSigner a i -> a -> m [RawData i]
+
+fetchItems ::
+  (ItemContainer a i, MonadHttp m, MonadThrow m, ShowId a) =>
+  Cli ->
+  ICOpt a i ->
+  ICSigner a i ->
+  a ->
+  m [i]
+fetchItems cli opt sig v =
+  fmap runParser
+    <$> fetchItemsRaw (pushHeader (showValId v) cli) opt sig v
