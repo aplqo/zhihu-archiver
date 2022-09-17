91b0c960c3e16e3b5fbba3dfcde6a882b90b1193
aplqo <aplqo@outlook.com>
2022-09-17 21:33:40 +0800

    [progress] truncate overlength message

--- src/ZhArchiver/Progress.hs
+++ src/ZhArchiver/Progress.hs
@@ -13,34 +13,26 @@ module ZhArchiver.Progress
 where
 
 import Control.Monad
+import Data.Foldable
 import GHC.IO.Handle
 import GHC.IO.Handle.FD
 
 data Cli = Cli
-  { cliMultiline, cliHeadTruncated :: Bool,
-    cliMaxHeader, cliHeaderCnt :: Int,
-    cliMsgHeader :: [String]
+  { cliMultiline :: Bool,
+    cliMaxWidth :: Int,
+    cliMsgHeader :: [(String, Int)]
   }
 
 defaultCli :: Cli
 defaultCli =
   Cli
     { cliMultiline = False,
-      cliHeadTruncated = False,
-      cliMaxHeader = 10,
-      cliHeaderCnt = 0,
+      cliMaxWidth = 80,
       cliMsgHeader = []
     }
 
 cliWithHeader :: String -> Cli
-cliWithHeader h =
-  Cli
-    { cliMultiline = False,
-      cliHeadTruncated = False,
-      cliMaxHeader = 10,
-      cliHeaderCnt = 1,
-      cliMsgHeader = [h]
-    }
+cliWithHeader h = pushHeader h defaultCli
 
 class ShowId a where
   showId :: a -> String
@@ -50,45 +42,51 @@ showValId :: (ShowId a) => a -> String
 showValId a = showType a ++ " " ++ showId a
 
 pushHeader :: String -> Cli -> Cli
-pushHeader s c@(Cli {cliMaxHeader = mx, cliHeaderCnt = cn, cliMsgHeader = h}) =
-  if cn == mx
-    then c {cliMsgHeader = s : tail h, cliHeadTruncated = True}
-    else c {cliHeaderCnt = cn + 1, cliMsgHeader = s : h}
+pushHeader s c@(Cli {cliMsgHeader = h}) = c {cliMsgHeader = (s, length s) : h}
 
 appendHeader :: String -> Cli -> Cli
 appendHeader s c =
   c
     { cliMsgHeader =
         let orig = cliMsgHeader c
+            l = length s
          in if null orig
-              then [s]
-              else (head orig ++ s) : tail orig
+              then [(s, l)]
+              else (let (s0, l0) = head orig in (s0 ++ s, l0 + l)) : tail orig
     }
 
 clearLine :: String
 clearLine = "\ESC[2K\ESC[G"
 
-showHeader :: Cli -> String
-showHeader Cli {cliMsgHeader = h, cliHeadTruncated = ht} =
+showMsg :: Cli -> String -> IO ()
+showMsg Cli {cliMsgHeader = h, cliMaxWidth = mx} msg =
   case h of
-    [] -> ""
-    x : xs ->
-      concat
-        ( reverse
-            ((\s -> ('[' : s) ++ "]") <$> xs)
-        )
-        ++ (if ht then "[...]" else "")
-        ++ x
-        ++ ": "
+    [] ->
+      if msgLen > mx
+        then putStr (take (mx - 3) msg) >> putStr ".."
+        else putStr msg
+    (x, l) : xs ->
+      if msgLen + l + 2 > mx
+        then traverse_ putStr [x, ": ", take (mx - l - 2 - 3) msg, "..."]
+        else truncateHdr 0 (mx - msgLen - 2 - l) (reverse xs) >> traverse_ putStr [x, ": ", msg]
+  where
+    msgLen = length msg
+    truncateHdr _ _ [] = pure ()
+    truncateHdr len lim ((s, l) : xs) =
+      if len + l + 2 + 5 > lim
+        then putStr "[...]"
+        else traverse_ putStr ["[", s, "]"] >> truncateHdr (len + 2 + l) lim xs
 
 showMessage :: Cli -> String -> IO ()
 showMessage c@(Cli {cliMultiline = m}) msg = do
-  putStr (clearLine ++ showHeader c ++ msg)
+  putStr clearLine
+  showMsg c msg
   when m $ putChar '\n'
 
 showProgress :: Cli -> String -> IO ()
 showProgress c msg = do
-  putStr (clearLine ++ showHeader c ++ msg)
+  putStr clearLine
+  showMsg c msg
   hFlush stdout
 
 endProgress :: Cli -> IO ()
